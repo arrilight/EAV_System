739bd1734589aa3e9ec7071c5ab6d965ce11564b
Georgiy Dzesov <g.dzesov@gmail.com>
2019-03-14 10:28:47 +0300

    add charging station package

---
+++ charging-station-package/modules/CS2VCommunication/CS2VCommunication.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Георгий Дзесов on 2019-03-14.
+//
+
+#include "CS2VCommunication.h"
+
+void CS2VCommunication::acceptConnection(int vehicleId) {
+
+}
+
+double CS2VCommunication::transferChargingRate(int vehicleId) {
+    return 0;
+}
+
+void CS2VCommunication::closeConnection(int vehicleId) {
+
+}
+
+void CS2VCommunication::earlyInterrupt(int vehicleId) {
+
+}

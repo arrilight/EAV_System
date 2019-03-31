b991e7bb58d40196a4200f4a87ef7381d250182c
Anastasia Reprintseva <anastasia.vorton@gmail.com>
2019-03-31 20:24:53 +0300

    add tinyxml library

---
+++ common/modules/XMLParser.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Anastasia Reprintseva on 2019-03-31.
+//
+
+#include "XMLParser.h"
+#include <tinyxml2.h>
---
+++ common/modules/XMLParser.h
@@ -0,0 +1,15 @@
+//
+// Created by Anastasia Reprintseva on 2019-03-31.
+//
+
+#ifndef EAV_SYSTEM_XMLPARSER_H
+#define EAV_SYSTEM_XMLPARSER_H
+
+
+class XMLParser {
+public:
+
+};
+
+
+#endif //EAV_SYSTEM_XMLPARSER_H
